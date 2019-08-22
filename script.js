67c3694362751cd0288f5febee4e21b58843ca75
Rajiv Ranjan <32765896+dreamingrajiv@users.noreply.github.com>
2019-08-22 15:54:58 +0530

    Wiki addition

---
+++ script.js
@@ -0,0 +1,28 @@
+var request = new XMLHttpRequest()
+
+request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
+request.onload = function() {
+  // Begin accessing JSON data here
+  var data = JSON.parse(this.response)
+
+  if (request.status >= 200 && request.status < 400) {
+    data.forEach(movie => {
+      console.log(movie.title)
+    })
+  } else {
+    console.log('error')
+  }
+}
+
+request.send()
+
+const app = document.getElementById('root')
+
+const logo = document.createElement('img')
+logo.src = 'logo.png'
+
+const container = document.createElement('div')
+container.setAttribute('class', 'container')
+
+app.appendChild(logo)
+app.appendChild(container)
